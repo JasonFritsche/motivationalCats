0ecdd86f0e3b5312ec6f03d2d88cfe84b36a4525
Supachai <group1034@gmail.com>
2019-10-30 14:20:42 +0700

    add loadng text and hide/show logic.

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ let dataAuthor;
 let dataQuoteText;
 let catUrl;
 
+let loadingIndicatorList = document.getElementsByClassName("loading-indicator");
+
 const getData = () => {
   getQuote();
   getCat();
 };
 
 const getQuote = () => {
+  setLoadingIndicator(true);
+  quoteContainer.style.display = 'none';
   fetch("https://quote-garden.herokuapp.com/quotes/random")
     .then(res => res.json())
     .then(data => {
@@ -19,24 +23,38 @@ const getQuote = () => {
       } else {
         quoteAuthor.innerHTML = dataAuthor;
       }
+      setLoadingIndicator(false);
+      quoteContainer.style.display = '';
     });
 };
 
 const getCat = () => {
+  setLoadingIndicator(true);
+  catImgSrc.style.display = 'none';
   fetch("https://aws.random.cat/meow")
     .then(res => res.json())
     .then(data => {
       catUrl = data.file;
       catImgSrc.src = catUrl;
+      setLoadingIndicator(false);
+      catImgSrc.style.display = '';
     });
 };
 
 let quoteText = document.getElementById("moteQuote");
 let quoteAuthor = document.getElementById("quoteAuthor");
 let catImgSrc = document.getElementById("catImg");
+let quoteContainer = document.getElementById("quote-container");
 
 const quoteButton = document.getElementById("quoteGetter");
 quoteButton.onclick = () => {
-  getCat();
-  getQuote();
+  getData();
 };
+
+
+setLoadingIndicator = (isShow=false) => {
+  for( let i=0; i<loadingIndicatorList.length ;i++ ){
+    let loading = loadingIndicatorList[i];
+    loading.style.display = isShow ? '' : 'none';
+  }
+}
