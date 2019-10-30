11c6e01dbe1add30813adc9a6987a631d8181c00
Jason F <fritsche.jason@gmail.com>
2019-10-29 20:56:05 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+let dataAuthor;
+let dataQuoteText;
+let catUrl;
+
+const getData = () => {
+	getQuote();
+	getCat();
+};
+
+const getQuote = () => {
+	fetch("https://quote-garden.herokuapp.com/quotes/random")
+		.then(res => res.json())
+		.then(data => {
+			dataAuthor = data.quoteAuthor;
+			dataQuoteText = data.quoteText;
+			quoteText.innerHTML = dataQuoteText;
+			quoteAuthor.innerHTML = dataAuthor;
+		});
+};
+
+const getCat = () => {
+	fetch("https://aws.random.cat/meow")
+		.then(res => res.json())
+		.then(data => {
+			catUrl = data.file;
+		});
+
+	catImgSrc.src = catUrl;
+	console.log(catImgSrc);
+};
+
+let quoteText = document.getElementById("moteQuote");
+let quoteAuthor = document.getElementById("quoteAuthor");
+let catImgSrc = document.getElementById("catImg");
+
+const quoteButton = document.getElementById("quoteGetter");
+quoteButton.onclick = () => {
+	getCat();
+	getQuote();
+};
