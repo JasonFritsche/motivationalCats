ee5e415f68e3494b1f64d5555b9e2b8278b83556
Kashif Rafique <kashif@logichub.fake>
2019-10-30 09:35:50 +0500

    Fix issue 2 & 5 and add some style

--- index.js
+++ index.js
@@ -1,38 +1,36 @@
 let dataAuthor;
 let dataQuoteText;
 let catUrl;
-
 const getData = () => {
 	getQuote();
 	getCat();
 };
-
 const getQuote = () => {
-	fetch("https://quote-garden.herokuapp.com/quotes/random")
-		.then(res => res.json())
-		.then(data => {
-			dataAuthor = data.quoteAuthor;
-			dataQuoteText = data.quoteText;
-			quoteText.innerHTML = dataQuoteText;
+	fetch("https://quote-garden.herokuapp.com/quotes/random").then(res => res.json()).then(data => {
+		dataAuthor = data.quoteAuthor;
+		dataQuoteText = data.quoteText;
+		quoteText.innerHTML = dataQuoteText;
+		if (dataAuthor === "") {
+			quoteAuthor.innerHTML = "Anonymous";
+		} else {
 			quoteAuthor.innerHTML = dataAuthor;
-		});
+		}
+	});
 };
-
 const getCat = () => {
-	fetch("https://aws.random.cat/meow")
-		.then(res => res.json())
-		.then(data => {
-			catUrl = data.file;
-		});
-
-	catImgSrc.src = catUrl;
+	fetch("https://aws.random.cat/meow").then(res => res.json()).then(data => {
+		catUrl = data.file;
+	});
+	if (catUrl === undefined) {
+		catImgSrc.src = "img/default-cat.jpg";
+	} else {
+		catImgSrc.src = catUrl;
+	}
 	console.log(catImgSrc);
 };
-
 let quoteText = document.getElementById("moteQuote");
 let quoteAuthor = document.getElementById("quoteAuthor");
 let catImgSrc = document.getElementById("catImg");
-
 const quoteButton = document.getElementById("quoteGetter");
 quoteButton.onclick = () => {
 	getCat();
