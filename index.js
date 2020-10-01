4b2b02973b0d4eb1ed7dd2070af9208174cb650b
Louise Findlay <louise@louisefindlay.com>
2020-10-01 12:44:59 +0100

    Added loader for quote and images

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let quoteText = document.getElementById('moteQuote');
 let quoteAuthor = document.getElementById('quoteAuthor');
 let catImgSrc = document.getElementById('catImg');
+let catLoader = document.getElementById('catLoader');
+let quoteLoader = document.getElementById('quoteLoader');
 
 const getData = () => {
     getQuote();
@@ -13,6 +15,7 @@ const getQuote = () => {
         .then((data) => {
             const dataAuthor = `- ${data.quoteAuthor}`;
             const dataQuoteText = `"${data.quoteText}"`;
+            quoteLoader.style.display = 'none';
             quoteText.innerHTML = dataQuoteText;
             if (dataAuthor === '') {
                 quoteAuthor.innerHTML = 'Anonymous';
@@ -28,6 +31,8 @@ const getCatImage = () => {
         .then((data) => {
             const catUrl = data[0].url;
             catImgSrc.src = catUrl;
+            catImgSrc.style.display = 'block';
+            catLoader.style.display = 'none';
         });
 };
 
