d62a7b2901d39c61aabed0e99cef223aa65587bf
Joao Vitor <jv.galvao14@gmail.com>
2021-10-03 20:41:34 -0300

    Updating the quote api URL and response handling

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-let quoteText = document.getElementById('moteQuote');
-let quoteAuthor = document.getElementById('quoteAuthor');
-let catImgSrc = document.getElementById('catImg');
-let loader = document.getElementById('loader')
+let quoteText = document.getElementById("moteQuote");
+let quoteAuthor = document.getElementById("quoteAuthor");
+let catImgSrc = document.getElementById("catImg");
+let loader = document.getElementById("loader");
 
 const activateLoader = () => {
-    loader.classList.add('activated');
+    loader.classList.add("activated");
 };
 
 const deactivateLoader = () => {
-    loader.classList.remove('activated');
+    loader.classList.remove("activated");
 };
 
 const getData = async () => {
@@ -17,47 +17,51 @@ const getData = async () => {
     await getQuote();
     await getCatImage();
 
-    if (catImgSrc.dataset.loading === 'false') {
+    if (catImgSrc.dataset.loading === "false") {
         deactivateLoader();
     }
 };
 
 const getQuote = async () => {
-    const res = await fetch('https://quote-garden.herokuapp.com/quotes/random');
-    const data = await res.json()
+    const res = await fetch(
+        "https://quote-garden.herokuapp.com/api/v3/quotes/random?genre=motivational"
+    );
+    const response = await res.json();
+    const data = response.data[0];
     const dataAuthor = `- ${data.quoteAuthor}`;
     const dataQuoteText = `"${data.quoteText}"`;
+
     quoteText.innerHTML = dataQuoteText;
-    if (dataAuthor === '') {
-        quoteAuthor.innerHTML = 'Anonymous';
+    if (dataAuthor === "") {
+        quoteAuthor.innerHTML = "Anonymous";
     } else {
         quoteAuthor.innerHTML = dataAuthor;
     }
 };
 
 const getCatImage = async () => {
-    const res = await fetch('https://api.thecatapi.com/v1/images/search')
-    const data = await res.json()
+    const res = await fetch("https://api.thecatapi.com/v1/images/search");
+    const data = await res.json();
     const catUrl = data[0].url;
 
     catImgSrc.src = catUrl;
     catImgSrc.dataset.loading = true;
 };
 
-const quoteButton = document.getElementById('quoteGetter');
+const quoteButton = document.getElementById("quoteGetter");
 quoteButton.onclick = () => {
     getData();
 };
 
-catImgSrc.addEventListener('load', () => {
+catImgSrc.addEventListener("load", () => {
     catImgSrc.dataset.loading = false;
-})
+});
 
-const imageObserver = new MutationObserver(mutations => {
-    mutations.forEach(mutationRecord => {
+const imageObserver = new MutationObserver((mutations) => {
+    mutations.forEach((mutationRecord) => {
         const { loading } = mutationRecord.target.dataset;
-        
-        if (loading === 'false') {
+
+        if (loading === "false") {
             deactivateLoader();
         }
     });
@@ -65,5 +69,5 @@ const imageObserver = new MutationObserver(mutations => {
 
 imageObserver.observe(catImgSrc, {
     attributes: true,
-    attributeFilter: ['data-loading'],
+    attributeFilter: ["data-loading"],
 });
