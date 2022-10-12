3b4f1ef02755d803bf30cde06bffde2f32ab21ec
anayatkhan1 <anayat0khan@gmail.com>
2022-10-12 22:16:12 +0530

    Adding Theme Toggle

--- index.js
+++ index.js
@@ -81,3 +81,19 @@ copyBtn.addEventListener("click", () => {
   }, 1000);
   });
 });
+
+// Theme Toggle 
+let icon = document.getElementById("icon");
+
+icon.onclick = function(){
+
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")) {
+
+      icon.src = "img/sun.png";
+    }
+
+    else {
+      icon.src = "img/moon.png";
+    }
+};
